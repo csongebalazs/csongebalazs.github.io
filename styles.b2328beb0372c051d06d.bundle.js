95af7c8f97d6dc3ae2ae7c4026885a6db6903d7c
csongebalazs <csongebalazs@gmail.com>
2017-01-04 09:57:50 +0100

    ready

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([3,4],{379:function(n,o){},915:function(n,o,c){n.exports=c(379)}},[915]);
+webpackJsonp([3,4],{380:function(n,o){},917:function(n,o,c){n.exports=c(380)}},[917]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
